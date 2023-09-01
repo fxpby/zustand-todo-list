59bbe2ed2ebfe0f527d7e33e97d533cb6c1cd311
fxpby <fxpby@outlook.com>
2023-09-01 18:19:08 +0800

    feat: init

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  proseWrap: 'never',
+}
