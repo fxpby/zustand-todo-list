70417322d354390278027b81d0dd393366e29205
fxpby <fxpby@outlook.com>
2023-09-05 15:39:00 +0800

    chore: add less config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+      },
+    },
+  },
 })
